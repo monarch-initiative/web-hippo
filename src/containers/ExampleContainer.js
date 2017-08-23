fbca5f11e44a7828bf7ad7c6ced4e7cd3eb4ac8e
Li Xu <lixu@garvanITs-MacBook-Pro.local>
2017-08-23 14:29:26 +1000

    added readme

--- src/containers/ExampleContainer.js
+++ src/containers/ExampleContainer.js
@@ -3,23 +3,30 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 import * as selectors from '../reducers';
 import Immutable from 'immutable';
+import { createSelector } from 'reselect'
 
 class ExampleContainer extends Component {
   render() {
     return (
-      <div style={{textAlign: 'center'}}>
+      <div style={{textAlign: 'center'}} className='container'>
         <h1>Home Page</h1>
+        <button onClick={this.props.addToState}> add </button>
       </div>
     );
   }
 }
 
-
 const mapStateToProps = (state, ownProps) => {
-  return {};
+  return createSelector(
+    selectors.getExampleData,
+    (exampleData) => ({exampleData})
+  )
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
+  addToState: () => {
+    dispatch(actions.fetchConsentTypeMappingRequest({gnenome: 'gen-value'}))
+  },
   dispatch
 });
 
