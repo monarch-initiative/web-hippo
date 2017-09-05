6c3cd06c3cb84d646588012947964438118ec7c4
Craig McNamara <craig.mcnamara@genome.one>
2017-09-05 14:45:16 +1000

    fix failing App.js test
    Problem: The App is wrapped in connect, so it make the default tests not work.
    
    Solution: Export the non-wrapped App and then do shallow a rendering in the test so that we don’t render any of the sub-components that are also connected.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Routes from './Routes';
 
-class App extends Component {
+export class App extends Component {
   render() {
     return (
       <div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const renderer = new ShallowRenderer();
+  renderer.render(<App />);
+  const result = renderer.getRenderOutput();
+  expect(!!result).toBe(true);
 });
