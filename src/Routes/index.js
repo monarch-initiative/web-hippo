e638d4bdd5206772f0d2486aaef928bf8a436fba
Craig McNamara <craig.mcnamara@genome.one>
2017-10-05 14:31:06 +1000

    wrap ensure logged in, with router properties
    Problem: EnsureLoggedInContainer wouldnt re-render because `connect`
    from redux would fight with the router.
    
    Solution: Wrap EnsureLoggedInContainer to router.

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import { Route, HashRouter } from 'react-router-dom';
+import { Route, HashRouter, withRouter } from 'react-router-dom';
 import ExampleContainer from '../containers/ExampleContainer';
 import EnsureLoggedInContainer from '../containers/EnsureLoggedInContainer';
+
+const EnsureLoggedInContainerWithRouter = withRouter(EnsureLoggedInContainer);
 
 class Routes extends Component {
   render() {
     return (
       <HashRouter>
-        <EnsureLoggedInContainer>
+        <EnsureLoggedInContainerWithRouter>
           <Route path="/" component={ExampleContainer} />
-        </EnsureLoggedInContainer>
+        </EnsureLoggedInContainerWithRouter>
       </HashRouter>
     );
   }
