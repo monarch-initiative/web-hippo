7ffd2d0e5194030434416276ffb5c10fa87ccfb1
Ali Azarkhish <ali.azarkhish@genome.one>
2017-12-13 13:13:49 +1100

    Adjusting authors view.

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -3,9 +3,16 @@ import { Label, Icon } from 'semantic-ui-react';
 
 export default function Authors({ authors }) {
   return (
-    <div style={{ paddingTop: 16, paddingBottom: 16 }}>
+    <div
+      style={{
+        paddingTop: 16,
+        paddingBottom: 16,
+        display: 'flex',
+        direction: 'row'
+      }}
+    >
+      <Icon name="users" />
       <Label.Group size="small">
-        <Icon name="users" />
         {authors.map((author, index) => (
           <Label key={index}>{`${author.lastName} ${author.initials}`}</Label>
         ))}
