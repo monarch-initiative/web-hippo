3b207d69065fb0992bce90a81fe24e95acbe175e
Ali Azarkhish <ali.azarkhish@genome.one>
2018-02-26 09:05:25 +1100

    GCP deployment.

--- scripts/build-for-demo.sh
+++ scripts/build-for-demo.sh
@@ -7,18 +7,16 @@ clean_app_name=${app_name:4}
 
 # Build for sandbox
 rm -rf build
-sed -i 's#"homepage": "/'$clean_app_name'"#"homepage": "/demo/'$bamboo_planRepository_name'/'"$branch_name"'"#g' package.json
-cat package.json
 
 REACT_APP_BASE_URL=/api/web-server REACT_APP_PASSPORT_URL=/demo/web-passport/develop CI=true npm run build
 mkdir -p $branch_name
 cp -R build/* $branch_name
 
 # SSH to sandbox
-ssh -i ~/.ssh/sandbox.pem ec2-user@13.55.2.207 /bin/bash <<EOF
-mkdir -p "/mnt/demo/$app_name/$branch_name"
+ssh 35.189.35.255 /bin/bash <<EOF
+mkdir -p "/var/nfs/demo/$app_name/$branch_name"
 exit
 EOF
 
 # Copy the files over
-scp -i ~/.ssh/sandbox.pem -r $branch_name/. ec2-user@13.55.2.207:/mnt/demo/$app_name/$branch_name/
+scp -r $branch_name/. 35.189.35.255:/var/nfs/demo/$app_name/$branch_name/
