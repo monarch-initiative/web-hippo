c7e9c868ab48febe65326a1087776e48e70d0877
Ali Azarkhish <ali.azarkhish@genome.one>
2018-02-26 09:17:10 +1100

    GCP deployment.

--- scripts/build-for-demo.sh
+++ scripts/build-for-demo.sh
@@ -13,13 +13,13 @@ mkdir -p $branch_name
 cp -R build/* $branch_name
 
 # SSH to sandbox
-ssh -i ~/.ssh/sandbox.pem ec2-user@13.55.2.207 /bin/bash <<EOF
-mkdir -p "/mnt/demo/$app_name/$branch_name"
+ssh 35.189.35.255 /bin/bash <<EOF
+mkdir -p "/var/nfs/demo/$app_name/$branch_name"
 exit
 EOF
 
 # Copy the files over
-scp -i ~/.ssh/sandbox.pem -r $branch_name/. ec2-user@13.55.2.207:/mnt/demo/$app_name/$branch_name/
+scp -r $branch_name/. 35.189.35.255:/var/nfs/demo/$app_name/$branch_name/
 
 # UAT!
 # Build for sandbox, but pointing to UAT
@@ -30,11 +30,11 @@ mkdir -p $branch_name
 cp -R build/* $branch_name
 
 # SSH to sandbox
-echo "Creating folder on Sandbox - /mnt/demo/uat/$app_name/$branch_name"
-ssh -i ~/.ssh/sandbox.pem ec2-user@13.55.2.207 /bin/bash <<EOF
-mkdir -p "/mnt/demo/uat/$app_name/$branch_name"
+echo "Creating folder on Sandbox - /var/nfs/demo/uat/$app_name/$branch_name"
+ssh 35.189.35.255 /bin/bash <<EOF
+mkdir -p "/var/nfs/demo/uat/$app_name/$branch_name"
 exit
 EOF
 
 # Copy the files over
-scp -i ~/.ssh/sandbox.pem -r $branch_name/. ec2-user@13.55.2.207:/mnt/demo/uat/$app_name/$branch_name/
+scp  -r $branch_name/. 35.189.35.255:/var/nfs/demo/uat/$app_name/$branch_name/
