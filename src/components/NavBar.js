c248c07292888cca17bc96c64325707eea3cf9a2
Ali Azarkhish <ali.azarkhish@genome.one>
2018-06-12 10:27:32 +1000

    Version update.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,7 +17,7 @@ export default function NavBar({ showFilters, onFiltersClick }) {
           </Menu.Item>
         )}
       </Container>
-      <Menu.Item>Hippo. Version 0.6.2 BETA</Menu.Item>
+      <Menu.Item>Hippo. Version 0.7.0 BETA</Menu.Item>
     </Menu>
   );
 }
